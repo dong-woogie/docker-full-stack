ec654e791d0406bb791a1cc9c38f2126d1ea2273
dong-woogie <com6511@gmail.com>
2021-12-08 20:37:34 +0900

    set environment variable

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "mysql",
-  user: "root",
-  password: "woogie777",
-  database: "myapp",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 exports.pool = pool;
